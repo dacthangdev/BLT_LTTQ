0b5a8a0237f584cf0688adba5b343e14be38769f
Your Name <dacthang163@gmail.com>
2022-10-30 00:31:26 +0700

    them file

---
+++ GiaoDienBaiTapLonBackUp/GiaoDienBaiTapLonBackUp/Classes/StaticData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiaoDienBaiTapLonBackUp.Classes
+{
+    internal class StaticData
+    {
+        public static string tenmon;
+    }
+}
--- GiaoDienBaiTapLonBackUp/GiaoDienBaiTapLonBackUp/FormMonAn.cs
+++ GiaoDienBaiTapLonBackUp/GiaoDienBaiTapLonBackUp/FormMonAn.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,6 +13,8 @@ namespace GiaoDienBaiTapLonBackUp
 {
     public partial class FormMonAn : Form
     {
+        Classes.ConnectData dta = new Classes.ConnectData();
+        string tenmon;
         public FormMonAn()
         {
             InitializeComponent();
@@ -20,6 +23,33 @@ namespace GiaoDienBaiTapLonBackUp
         private void iconbtnClose_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void FormMonAn_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                tenmon = Classes.StaticData.tenmon;
+                DataTable dataTable = dta.ReadData("select * from monan where tenmon = N'" + tenmon + "'");
+                string fileanh = dataTable.Rows[0]["hinhanh"].ToString();
+                lblMota.Text = dataTable.Rows[0]["mota"].ToString();
+                lblGiathanh.Text = dataTable.Rows[0]["giatien"].ToString();
+                ptbMonan.Image = Image.FromFile(Application.StartupPath + "\\Images\\" + fileanh);
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void iconbtnShip_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void iconbtnGoiMon_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
